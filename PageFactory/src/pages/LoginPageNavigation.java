e44ce72c225c970e97f74157f9cf0a5b6d63791a
divlakjune18 <divlakjune18@gmail.com>
2021-09-11 12:53:55 +0530

    divya feature update

--- PageFactory/src/pages/LoginPageNavigation.java
+++ PageFactory/src/pages/LoginPageNavigation.java
@@ -12,27 +12,27 @@ public class LoginPageNavigation {
 
 			WebElement signIn = driver.findElement(By.xpath("//*[@class='login']"));
 			signIn.click();
-
+			System.out.println("Divya Code");
 		}
 
 		public static void enterUsername(WebDriver driver) {
 			WebElement username = driver.findElement(By.xpath("//*[@id='email']"));
-
 			username.sendKeys("kishorevelraj95@gmail.com");
-
+			System.out.println("Divya Code");
 		}
 
 		public static void enterPassword(WebDriver driver) {
 			WebElement passwords = driver.findElement(By.xpath("//*[@id='passwd']"));
-
 			passwords.sendKeys("12345");
+			System.out.println("Divya Code");
 		}
 
 		public static void clickSignInButton(WebDriver driver) {
 			WebElement loginbutton = driver.findElement(By.xpath("//*[@id='SubmitLogin']"));
 			loginbutton.click();
-
+			System.out.println("Divya Code");
 		}
+		
 
 
 	}
